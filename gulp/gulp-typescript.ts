c85a6ca15aed0d90a48611800e2b06a768bec959
grahbari <guillaume.rahbari@amadeus.com>
2016-03-08 15:31:42 +0100

    typescript task

---
+++ gulp/gulp-typescript.ts
@@ -0,0 +1,34 @@
+import * as gulp from 'gulp';
+import * as gulpLoadPlugins from 'gulp-load-plugins';
+
+const plugins = <any>gulpLoadPlugins();
+
+let _tsProject = plugins.typescript.createProject('tsconfig.json', {
+    typescript : require('typescript')
+});
+
+let typings = ['manual-typings/manual-typings.d.ts','typings/browser.d.ts'];
+
+/**
+ * This function transpiles typescript files.
+ * @param {Array} filesArray - The files to be transpiled.
+ * @param {String} destDirectory - The destination directory.
+ */
+function compile(filesArray, destDirectory) {
+    return gulp.src(filesArray)
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.typescript(_tsProject))
+        .pipe(plugins.sourcemaps.write('./'))
+        .pipe(gulp.dest(destDirectory));
+}
+
+/**
+ * This function transpiles typescript files into the dist directory.
+ */
+function dist() {
+    return compile(typings.concat(['src/**/*.e2e.ts']), './dist/');
+}
+
+///////////////////// Typescript Tasks /////////////////////
+
+gulp.task('ts:dist', dist);

cbe51ae38a498c99134c06df409c4e1d0fa123c4
grahbari <guillaume.rahbari@amadeus.com>
2016-03-08 17:01:37 +0100

    serveur  task

---
+++ gulp/gulp-serve.ts
@@ -0,0 +1,20 @@
+import * as gulp from 'gulp';
+import * as browserSync from 'browser-sync';
+
+// We can reuse this server in other files.
+let bs = browserSync.create('Server');
+
+/**
+ * This function initiates the server.
+ */
+function init(){
+    bs.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+}
+
+///////////////////// Copy Tasks /////////////////////
+
+gulp.task('server:init', init);

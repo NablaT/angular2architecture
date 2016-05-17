8422c98e3f07ecd18115d07734f986b8103ee27a
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-05-17 16:51:58 +0200

    gulp copy

--- gulp/gulp.conf.js
+++ gulp/gulp.conf.js
@@ -5,6 +5,22 @@
  */
 
 /**
+ * @constant {String[]} Constant containing all path to shims dependencies we want in the project.
+ */
+export const SHIMS_DEPENDENCIES = [
+    'node_modules/es6-shim/es6-shim.min.js'
+];
+
+/**
+ * @constant {String[]} Constant containing all path to libraries dependencies we want in the project.
+ */
+export const LIBS_DEPENDENCIES = [
+    'node_modules/zone.js/dist/zone.js',
+    'node_modules/reflect-metadata/Reflect.js',
+    'node_modules/systemjs/dist/system.src.js'
+];
+
+/**
  * @constant {String} The path of the development directory.
  */
 export const DEV_PATH = 'dist/dev';
@@ -13,3 +29,38 @@ export const DEV_PATH = 'dist/dev';
  * @constant {String} The path of the production directory.
  */
 export const PROD_PATH = 'dist/prod';
+
+/**
+ * @constant {String} Regular expression matching all files.
+ */
+export const ALL_FILES = 'src/**/*';
+
+/**
+ * @constant {String} Regular expression matching all typescript files.
+ */
+export const TYPESCRIPT_FILES = 'src/**/*.ts';
+
+/**
+ * @constant {String} Regular expression matching all sass files.
+ */
+export const SASS_FILES = 'src/**/*.scss';
+
+/**
+ * @constant {String} Regular expression matching all spec files.
+ */
+export const SPEC_FILES = 'src/**/*.spec.ts';
+
+/**
+ * @constant {String} The name of the index.
+ */
+export const INDEX = 'index.html';
+
+/**
+ * @constant {String} The libs tag name.
+ */
+export const LIBS = 'libs';
+
+/**
+ * @constant {String} The shim tag name.
+ */
+export const SHIMS = 'shims';
---
+++ gulp/tasks/gulp-copy.js
@@ -0,0 +1,58 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import { getBrowserSync } from '../browsersync';
+import {
+    DEV_PATH,
+    PROD_PATH,
+    ALL_FILES,
+    TYPESCRIPT_FILES,
+    SASS_FILES,
+    INDEX
+} from '../gulp.conf';
+
+const plugins = gulpLoadPlugins();
+
+let bs = getBrowserSync();
+
+/**
+ * @constant The array of excludes files.
+ */
+const EXCLUDED_FILES = [
+    '!' + TYPESCRIPT_FILES,
+    '!' + SASS_FILES
+];
+
+/**
+ * This function copies ALL_FILES excepts:
+ * <ul>
+ *   <li>Typescript files</li>
+ *   <li>Sass files</li>
+ * </ul>
+ * into the destinationDirectory directory.
+ *
+ * @param {String} files - Files to copy.
+ * @param {String} destinationDirectory - The destination directory.
+ */
+function copyDist (files, destinationDirectory) {
+
+    const FILES = [files].concat(EXCLUDED_FILES);
+
+    return gulp.src(FILES, {base: 'src'})
+               .pipe(plugins.changed(destinationDirectory))
+               .pipe(gulp.dest(destinationDirectory))
+               .pipe(plugins.if(files !== ('src/' + INDEX), bs.stream()));
+}
+
+///////////////////// Copy Tasks /////////////////////
+
+gulp.task('copy:dev', () => {
+    return copyDist(ALL_FILES, DEV_PATH);
+});
+
+gulp.task('copy:prod', () => {
+    return copyDist(ALL_FILES, PROD_PATH);
+});
+
+gulp.task('copy:index', () => {
+    return copyDist('src/' + INDEX, DEV_PATH);
+});

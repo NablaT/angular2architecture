09ad8d199a97a9a513de2a45cadb586de6c5a7dc
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-05-17 17:17:00 +0200

    gulp watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,15 +3,18 @@ import HubRegistry from 'gulp-hub';
 
 const hub = new HubRegistry(['gulp/tasks/*.js']);
 
-gulp.task('serve', callback =>
-    runSequence('build', 'watch', callback)
+gulp.task('serve', callback => {
+    gulp.series('build:dev', 'watch', callback);
+});
+
+gulp.task('serve:prod', callback => {
+    gulp.series('build:prod', 'server:prod', callback)
+});
+
+gulp.task('build:dev', callback => {
+    gulp.series('clean:dev', gulp.parallel('copy:dev', 'ts:dev', 'sass:dev'), 'inject:dev', 'server:dev', callback)
+});
+
+gulp.task('build:prod', callback =>
+    gulp.series('clean:prod', gulp.parallel('copy:prod', 'ts:prod', 'sass:prod'), 'inject:prod', 'useref:prod', callback)
 );
-
-gulp.task('build', callback =>
-    runSequence('clean', ['copy:dist', 'ts:dist', 'sass:dist'], 'inject', 'server:init', callback)
-);
-
-/*var build = gulp.series('clean');
-export {build};
-
-export default build;*/

f256e379cbde265b267ddd0ecaf37fe5183429fb
grahbari <guillaume.rahbari@amadeus.com>
2016-05-24 15:42:28 +0200

    build html

---
+++ gulp/tasks/build.html.dev.js
@@ -0,0 +1,11 @@
+import gulp from 'gulp';
+import path from 'path';
+import {
+    DEV_DIR,
+    APP_SRC
+} from '../gulp.conf';
+
+gulp.task('build:html:dev', () => {
+    return gulp.src(path.join(APP_SRC, '**', '*.html'))
+               .pipe(gulp.dest(path.join(DEV_DIR, 'app')));
+});
---
+++ gulp/tasks/build.html.prod.js
@@ -0,0 +1,11 @@
+import gulp from 'gulp';
+import path from 'path';
+import {
+    TMP_DIR,
+    APP_SRC
+} from '../gulp.conf';
+
+gulp.task('build:html:prod', () => {
+    return gulp.src(path.join(APP_SRC, '**', '*.html'))
+               .pipe(gulp.dest(path.join(TMP_DIR, 'app')));
+});

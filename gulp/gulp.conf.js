3fa8292a173423cf221f7c43d0bc969870a8a992
grahbari <guillaume.rahbari@amadeus.com>
2016-05-26 11:44:00 +0200

    Refactor

--- gulp/gulp.conf.js
+++ gulp/gulp.conf.js
@@ -6,24 +6,7 @@
 
 import path from 'path';
 
-/**
- * @constant {String[]} Constant containing all path to shims dependencies we want in the project.
- */
-export const SHIMS_DEPENDENCIES = [
-    'node_modules/core-js/client/shim.min.js'
-];
-
-/**
- * @constant {String[]} Constant containing all path to libraries dependencies we want in the project.
- */
-export const LIBS_DEPENDENCIES = [
-    'node_modules/zone.js/dist/zone.js',
-    'node_modules/reflect-metadata/Reflect.js',
-    'node_modules/systemjs/dist/system.src.js'
-];
-
-///////////////////// Directories Paths /////////////////////
-
+export const INDEX        = 'index.html';
 export const DIST_DIR     = 'dist';
 export const SRC_DIR      = 'src';
 export const APP_SRC      = path.join(SRC_DIR, 'app');
@@ -33,9 +16,6 @@ export const DEV_DIR      = path.join(DIST_DIR, 'dev');
 export const PROD_DIR     = path.join(DIST_DIR, 'prod');
 export const TMP_DIR      = path.join(DIST_DIR, 'tmp');
 export const TEMPLATE_DIR = path.join(DIST_DIR, 'template');
-
-export const ALL_FILES        = 'src/**/*';
-export const TYPESCRIPT_FILES = 'src/**/*.ts';
 
 const NPM_DEPENDENCIES = [
     {src: 'node_modules/core-js/client/shim.min.js', inject: 'shims'},
@@ -49,28 +29,3 @@ const CSS_INJECTABLES = [
 ];
 
 export const DEPENDENCIES = NPM_DEPENDENCIES.concat(CSS_INJECTABLES);
-
-/**
- * @constant {String} Regular expression matching all sass files.
- */
-export const SASS_FILES = 'src/**/*.scss';
-
-/**
- * @constant {String} Regular expression matching all spec files.
- */
-export const SPEC_FILES = 'src/**/*.spec.ts';
-
-/**
- * @constant {String} The name of the index.
- */
-export const INDEX = 'index.html';
-
-/**
- * @constant {String} The libs tag name.
- */
-export const LIBS = 'libs';
-
-/**
- * @constant {String} The shim tag name.
- */
-export const SHIMS = 'shims';

61fa57fb66a41934b9851da26d20c2aa03849c88
grahbari <guillaume.rahbari@amadeus.com>
2016-05-26 09:33:10 +0200

    Update angular

--- gulp/tasks/dev/server.dev.js
+++ gulp/tasks/dev/server.dev.js
@@ -2,7 +2,6 @@ import gulp from 'gulp';
 import {DEV_DIR} from '../../gulp.conf';
 import {init} from '../../utils/browsersync';
 
-gulp.task('server:dev', (done) => {
+gulp.task('server:dev', () => {
     init(DEV_DIR);
-    done();
 });
